3db0a6da3ca61328be96e9e63540d51c03610c0e
AyloSrd <aylo.srd@gmail.com>
2022-04-13 15:30:32 +0200

    vfs works

--- src/tools/esbuild-tools.ts
+++ src/tools/esbuild-tools.ts
@@ -1,13 +1,21 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
 
+interface VFS {
+    [key: string]: string
+}
+
+const ENTRY_POINT_JSX = 'App.jsx'
+
 const AppdefaultContent = `
-import React, { useState } from 'react';
-console.log(React, useState);
+import React, { useState } from 'react'
+import { test } from './Counter.jsx'
+console.log(test);
 `.trim()
 
-const vfs = {
-    'App.jsx': AppdefaultContent
+const vfs: VFS = {
+    [ENTRY_POINT_JSX]: AppdefaultContent,
+    'Counter.jsx': 'export const test = "yo"'
 }
 
 export const unpkgPathPlugin = () => {
@@ -16,8 +24,15 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        if (args.path === 'App.jsx') {
+        if (args.path === ENTRY_POINT_JSX) {
           return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.startsWith('./') && vfs[args.path.substring(2)]) {
+            return {
+                namespace: 'a',
+                path: args.path.substring(2)
+            }
         }
 
         if (args.path.includes('./') || args.path.includes('../')) {
@@ -39,12 +54,19 @@ export const unpkgPathPlugin = () => {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
 
-        if (args.path === 'App.jsx') {
+        if (args.path === ENTRY_POINT_JSX) {
           return {
             loader: 'jsx',
-            contents: vfs['App.jsx'],
+            contents: vfs[ENTRY_POINT_JSX],
           };
         }
+
+        if (vfs[args.path]) {
+            return {
+              loader: 'jsx',
+              contents: vfs[args.path],
+            };
+          }
 
         const { data, request } = await axios.get(args.path);
         return {
