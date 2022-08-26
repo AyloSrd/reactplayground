3b1f84d9c0af1108c9b7937e427ef69b8cc063dd
AyloSrd <aylo.srd@gmail.com>
2022-08-26 17:28:46 +0200

    compiler + exports work

--- src/hooks/playground/useEsbuild.ts
+++ src/hooks/playground/useEsbuild.ts
@@ -20,6 +20,10 @@ interface OutputTypeFail {
 
 export type OutputType = OutputTypeSuccess | OutputTypeFail
 
+export const CDN = 'https://esm.sh'
+
+export const make_CDN_URL = (pkg: string) => `${CDN}/${pkg}?dev?pin=v92`
+
 const fileCache = localforage.createInstance({
     name: 'filecache',
 })
@@ -44,9 +48,9 @@ export default function useEsbuild(vfsFromUrl: VFS | null) {
     const versionGeneratorRef = useRef<Generator<number>>(countGen())
     const versionRef = useRef<number>(versionGeneratorRef.current.next().value)
 
-    const unpkgPathPlugin = useCallback((vfs: VFS) => {
+    const vfs_with_esm_sh_plugin = useCallback((vfs: VFS) => {
         return {
-            name: 'unpkg-path-plugin',
+            name: 'vfs-with-esm-sh-plugin',
             setup(build: esbuild.PluginBuild) {
                 build.onResolve({ filter: /.*/ }, async (args: any) => {
 
@@ -68,19 +72,35 @@ export default function useEsbuild(vfsFromUrl: VFS | null) {
                         }
                     }
 
+                    if (args.path.startsWith(CDN)) {
+                        return {
+                        namespace: 'b',
+                        path: args.path,
+                        }
+                    }
+
                     if (args.path.includes('./') || args.path.includes('../')) {
                         return {
                         namespace: 'b',
                         path: new URL(
                             args.path,
-                            'https://unpkg.com' + args.resolveDir + '/'
+                            CDN + args.resolveDir + '/'
                         ).href,
+                        }
+                    }
+
+                    if (args.path.startsWith('/')) {
+                        return {
+                            namespace: 'b',
+                            //@ts-ignore: defineHack is defined in index.html
+                            path: `${CDN}${args.path}`,
                         }
                     }
 
                     return {
                         namespace: 'b',
-                        path: `https://unpkg.com/${args.path}`,
+                        //@ts-ignore: defineHack is defined in index.html
+                        path: make_CDN_URL(args.path),
                     }
                 })
 
@@ -130,11 +150,12 @@ export default function useEsbuild(vfsFromUrl: VFS | null) {
         }
         try {
             const bundle = await esbuildRef.current.build({
-                entryPoints: [ENTRY_POINT_JSX],
                 bundle: true,
+                entryPoints: [ENTRY_POINT_JSX],
+                format: 'esm',
                 metafile: true,
                 write: false,
-                plugins: [unpkgPathPlugin(vfs)],
+                plugins: [vfs_with_esm_sh_plugin(vfs)],
                 // @ts-ignore, this is necessary because vite will automatically escape and replace the string "process.env.NODE_ENV"
                 define: window.defineHack,
             })
@@ -168,6 +189,12 @@ export default function useEsbuild(vfsFromUrl: VFS | null) {
         } catch {
             createBundle(vfs, versionRef.current)
         }
+
+        function clearDB() {
+            localforage.clear()
+        }
+
+        return () => clearDB()
     }, [])
 
     return {
